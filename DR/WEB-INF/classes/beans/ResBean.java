e2cbc52b4c7c69a3bd8566ca1a6a046ac4c3688b
tk18304008 <tk18304008@ga.tera-house.ac.jp>
2019-02-19 17:24:10 +0900

    no message

--- DR/WEB-INF/classes/beans/ResBean.java
+++ DR/WEB-INF/classes/beans/ResBean.java
@@ -13,85 +13,85 @@ public class ResBean{
 	 /**
      * @param thread_id the res_id to set
      */
-    public void setRes_id(int res_id) {
+    public void setResId(int res_id) {
         this.res_id = res_id;
     }
     /**
      * @return the res_id
      */
-    public int getRes_id() {
+    public int getResId() {
         return res_id;
     }
     /**
      * @param thread_id the thread_id to set
      */
-    public void setThread_id(int thread_id) {
+    public void setThreadId(int thread_id) {
         this.thread_id = thread_id;
     }
     /**
      * @return the thread_id
      */
-    public int getThread_id() {
+    public int getThreadId() {
         return thread_id;
     }
     /**
      * @param thread_name the res_text to set
      */
-    public void setRes_text(String res_text) {
+    public void setResText(String res_text) {
         this.res_text = res_text;
     }
     /**
      * @return the res_text
      */
-    public String getRes_text() {
+    public String getResText() {
         return res_text;
     }
     /**
      * @param res_name the res_name to set
      */
-    public void setRes_name(String res_name) {
+    public void setResName(String res_name) {
         this.res_name = res_name;
     }
     /**
      * @return the thread_date
      */
-    public String getRes_name() {
+    public String getResName() {
         return res_name;
     }
     /**
      * @param res_good the res_good to set
      */
-    public void setRes_good(int res_good) {
+    public void setResGood(int res_good) {
         this.res_good = res_good;
     }
     /**
      * @return the res_good
      */
-    public int getRes_good() {
+    public int getResGood() {
         return res_good;
     }
     /**
      * @param res_date the res_date to set
      */
-    public void setRes_date(String res_date) {
+    public void setResDate(String res_date) {
         this.res_date = res_date;
     }
     /**
      * @return the res_date
      */
-    public int getRes_date() {
+    public int getResDate() {
         return res_date;
     }
     /**
      * @param res_number the res_number to set
      */
-    public void setRes_number(int res_number) {
+    public void setResNumber(int res_number) {
         this.res_number = res_number;
     }
     /**
      * @return the res_number
      */
-    public int getRes_number() {
+    public int getResNumber() {
         return res_number;
     }
     /**
