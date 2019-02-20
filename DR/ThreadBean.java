1a57d76df4d39c6b72706d2a032d73b0b3574239
nim-oi <44827625+nim-oi@users.noreply.github.com>
2019-02-20 17:08:58 +0900

    Create ThreadBean.java

---
+++ DR/ThreadBean.java
@@ -0,0 +1,50 @@
+package beans;
+
+public class ThreadBean{
+    private String threadname;
+    private String category;
+    private String resname;
+    private String threadtext;
+    private String threadid;
+
+    public void setThreadname(String threadname){
+        this.threadname = threadname;
+    }
+
+    public String getThreadname(){
+        return threadname;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setResname(String resname){
+        this.resname = resname;
+    }
+
+    public String getResname(){
+        return resname;
+    }
+
+    public void setThreadtext(String threadtext){
+        this.threadtext = threadtext;
+    }
+
+    public String getThreadtext(){
+        return threadtext;
+    }
+
+    public void setThreadid(String threadid){
+        this.threadid = threadid;
+    }
+
+    public String getThreadid(){
+        return threadid;
+    }
+
+}
